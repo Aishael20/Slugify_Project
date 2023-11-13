1aa554bebf09811fc0e388993369c8e7a4e59168
aishael <aishadona10@gmail.com>
2023-11-13 14:07:10 -0500

    Nou commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+
+setup(name="slug",description="Un projet python permettant de convertir n'importe quelle chaine de caractère en slug",
+      author="Picard Aïshael ",
+      author_email="aishael.picard@gmail.com",
+      maintainer='borgellas Samuel',
+    maintainer_email='borgellassamuel@gmail.com',
+    url="",
+    version="0.1",packages="find_packages()",
+    entry_points={},
+
+    install_requires=[], classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+
+
+      )
