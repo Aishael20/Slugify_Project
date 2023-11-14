7b732cf0e5c7f1f7c4bb8191418805b77a7c8d69
aishael <aishadona10@gmail.com>
2023-11-14 13:47:29 -0500

    ok

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
-setup(name="Slug",description="Un projet Python qui permet de convertir n'importe quelle chaîne de caractères en slug ",
-      authors=["Borgellas Samuel","Picard Aïshael "],
-      author_email=["aishael.picard@gmail.com","borgellassamuel@gmail.com"],
-      
-    url="https://github.com/Aishael20/Slugify_Project/settings/access?guidance_task=",
-    version="0.1",packages="find_packages()",
+setup(
+    name="Slug",
+    description="Un projet Python qui permet de convertir n'importe quelle chaîne de caractères en slug",
+    author="Borgellas Samuel and Picard Aïshael",
+    author_email='aishael.picard@gmail.com and borgellassamuel@gmail.com',
+    url="https://github.com/Aishael20/Slugify_Project",
+    version="0.1",
+    packages=find_packages(),
     entry_points={},
-
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -16,8 +17,6 @@ setup(name="Slug",description="Un projet Python qui permet de convertir n'import
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: Implementation :: PyPy'
     ]
-
-
-      )
+)
